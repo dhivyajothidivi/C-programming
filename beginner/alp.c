c89adec4ca055353efab89a9d3838b50d75f786c
dhivyajothidivi <38652848+dhivyajothidivi@users.noreply.github.com>
2018-04-24 10:04:06 +0000

    Add files via upload

---
+++ beginner/alp.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{ 
+	char n;
+ 	scanf("%s",&n);
+	if((n>='a'&& n<='z')||(n>='A'&& n<='Z'))
+	{
+	printf("Alphabet");
+	}
+	else
+	{
+	printf("Not");
+	}
+	return 0;
+}
