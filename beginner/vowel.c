39869dd01837ca941b527df2da86e9e137fcfa2f
dhivyajothidivi <38652848+dhivyajothidivi@users.noreply.github.com>
2018-04-24 09:47:40 +0000

    Add files via upload

---
+++ beginner/vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{ 
+	char n;
+ 	scanf("%s",&n);
+	if(n=='a'||n=='e'||n=='i'||n=='o'||n=='u')
+	{
+	printf("Vowel");
+	}
+	else
+	{
+	printf("Constenant");
+	}
+	return 0;
+}
