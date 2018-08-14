e75f5886eaced1c74b0afa9549da1c02bb6d46ba
dhivyajothidivi <38652848+dhivyajothidivi@users.noreply.github.com>
2018-08-14 10:30:59 +0000

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int n,k,i,a[10];
+	int sum=0;
+	scanf("%d%d",&n &k);
+	for(i=1;i<=n;i++)
+	{
+		printf("%d",a[10]);
+	}
+	for(i=1;i<=k;i++)
+	{
+		sum=sum+i;
+	}
+	printf("%d",sum);
+}
